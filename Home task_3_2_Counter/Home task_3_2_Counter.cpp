352815757725f00ba65df3d6b2bfd3431d9a23fc
Mikhail <miktyu88@gmail.com>
2024-01-16 00:15:19 +0300

    Correction 2

--- Home task_3_2_Counter/Home task_3_2_Counter.cpp
+++ Home task_3_2_Counter/Home task_3_2_Counter.cpp
@@ -39,55 +39,33 @@ int main(int argc, char** argv)
     std::string init_count_choice;
     char command;
 
+    Counter count;
     std::cout << "Вы хотите указать начальное значение счётчика? Введите 'yes' или 'no': ";
     std::cin >> init_count_choice;
     if (init_count_choice == yes) {
         std::cout << "Введите начальное значение счётчика: ";
         std::cin >> custom_count;
-        Counter C1(custom_count);
-        do {
-            std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
-            std::cin >> command;
-            
-            switch (command) {
-            case(incr):
-                C1.increment();
-                break;
-            case(dcr):
-                C1.decrement();
-                break;
-            case(gt):
-                std::cout << C1.getCounter() << std::endl;
-                break;
-            default:
-                break;
-            }
-        } while (command != hlt);
-
+        count = Counter(custom_count);
     }
-    else {
-        Counter C2; 
-        do {
-            std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
-            std::cin >> command;
-
-            switch (command) {
+    do {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> command;
+            
+        switch (command) {
             case(incr):
-                C2.increment();
+                count.increment();
                 break;
             case(dcr):
-                C2.decrement();
+                count.decrement();
                 break;
             case(gt):
-                std::cout << C2.getCounter() << std::endl;
+                std::cout << count.getCounter() << std::endl;
                 break;
             default:
                 break;
             }
         } while (command != hlt);
-
-    };
-
+    
     std::cout << "До свидания!" << std::endl;
 
     return 0;
